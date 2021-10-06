cfda927fbc92af98313546ab1f37bb59800057ac
jordanthopps <91483280+jordanthopps@users.noreply.github.com>
2021-10-06 13:48:42 -0400

    Self-code

--- js/site.js
+++ js/site.js
@@ -1,16 +1,66 @@
-function displayMessage() {
-    let msg = ""
+//get starting values from the screen
+//Controller Function
+function getUserString() {
+    //Get the user's input
 
-    msg = document.getElementById("message").value;
+    let userString = document.getElementById("userString").value
+    //Reverse the user's input - separate function
 
-    alert(msg);
+    let revString = reverseString(userString);
+
+    //Compare original input and reversed input - separate function
+    let palindrome = compareStrings(userString, revString);
+
+    //Output Result - separate function
+    displayData(palindrome)
+
 }
 
-function displayMessage2() {
-    let msg = "";
+function reverseString(userString) {
+    //This is Rewind
+    let firstLetter = userString.length - 1
+    let lastLetter = 0
 
-    msg = document.getElementById("message").value;
+    let revString = ""
 
-    Swal.fire(msg);
+    for (let i = firstLetter; i >= lastLetter; i--) {
+
+        revString = revString + userString[i]
+    }
+
+    return revString
+};
+
+function compareStrings(userString, revString) {
+    //This is the work part
+    //A != a
+    //Normalize the case
+    //Extra credit - "A man, a plan, a canal; Panama" - is a palindrome when the spaces and special character are removed
+    //RegEx is used for this - Google is your friend (regular expression. use a generator)
+
+    let normalUserString = userString.toLowerCase();
+    let normalRevString = revString.toLowercase();
+
+    let returnedUserIn = "You entered " + userString;
+    let returnedRevString = "The reverse " + revString;
+    let Winner = "That's a palindrome!";
+    let Loser = "Great effort - try again!";
+
+    if (normalUserString === normalRevString) {
+        return palindrome = returnedUserIn, returnedRevString, Winner;
+    } else if (normalUserString !== normalRevString) {
+        return palindrome = returnedUserIn, returnedRevString, Loser;
+    }
+};
+
+function displaydata(palindrome) {
+    //We want to output three things
+    //1. The original input
+    //2. The reversed input
+    //3. was it a palindrome
+
+    let output = document.getElementById("Results");
+
+    output.innerHTML = palindrome;
 
 }
